c8a9ef04ed1e3ec0ed5ef85b220e1ce2de77d626
Ziye <504250110@qq.com>
2021-10-11 18:08:32 +0800

    update

--- util/spider.py
+++ util/spider.py
@@ -10,6 +10,18 @@ from queue import Queue
         1.中断后从断点开始爬取
         2.爬取关注的人
 """
+
+def repeat_request(url):
+    response = requests.request("GET", url)
+    data = response.content
+    data = json.loads(data)
+    if data['ok'] == 0:
+        return data, False
+    elif 'user' not in data['data']['cards'][-1]['card_group'][0].keys():
+        return data, False
+    else:
+        return data, True
+
 def spider_fans(user_id, since_id):
     fans_info = []
 
@@ -20,19 +32,15 @@ def spider_fans(user_id, since_id):
     # cookie = "WEIBOCN_FROM=1110006030; SUB=_2A25MZdyQDeRhGeFO7VAY9y3EyTqIHXVvqeTYrDV6PUJbkdANLXHNkW1NQU89GizFRnsnG85Yx52afW4ioSYTBWNI; MLOGIN=1; _T_WM=73123320612; XSRF-TOKEN=c8dece; M_WEIBOCN_PARAMS=luicode=10000011&lfid=231051_-_fans_-_3479691367&fid=231051_-_fans_-_3479691367&uicode=10000011"
     # headers = {'cookie': cookie}
 
-    response = requests.request("GET", url)
-    data = response.content
-    data = json.loads(data)
+    data, success_flag = repeat_request(url)
 
     # 返回数据为空时，repeat
     wait_time = 0.5859375
-    while data['ok'] == 0:
+    while not success_flag:
         print("Request Failed!!!\tWait {}s".format(wait_time))
         time.sleep(wait_time)
-        response = requests.request("GET", url)
-        data = response.content
-        data = json.loads(data)
-        if wait_time >= 600:
+        data, success_flag = repeat_request(url)
+        if wait_time >= 37.5:
             return [], True
         elif wait_time <= 150:
             wait_time *= 4
@@ -48,7 +56,9 @@ def spider_fans(user_id, since_id):
     for fan in fans_list:
         try:
             # 忽略粉丝数>500的用户
-            if fan['user'] is not None and int(fan['user']['followers_count']) <= 500:
+            if fan['user'] is not None and \
+                    fan['user']['followers_count'].isdigit() and \
+                    int(fan['user']['followers_count']) <= 500:
                 try:
                     # id、昵称、性别
                     fans_info.append({'fan_id': fan['user']['id'], 'fan_name': fan['user']['screen_name'],
@@ -56,7 +66,8 @@ def spider_fans(user_id, since_id):
                 except:
                     print("Error!!!", fan)
                     continue
-        except ValueError:
+        except:
+            print("Error!!!", fan)
             continue
     return fans_info, end
 
@@ -64,12 +75,12 @@ def spider_fans(user_id, since_id):
 user_ids = Queue()
 temp_user_ids = Queue()
 fans_info = []
-user_info_cache = {}
+visited_user_cache = {}
 user_ids.put(3479691367)
 queue_len = user_ids.qsize()
 search_depth = 0
 
-while search_depth <= 1:
+while search_depth <= 5:
     if user_ids.empty():
         # 本层遍历结束
         user_ids = temp_user_ids
@@ -89,9 +100,13 @@ while search_depth <= 1:
         search_depth += 1
     else:
         user_id = user_ids.get()
-        try:
-            fans_list = user_info_cache[user_id]
-        except KeyError:
+        if user_id in visited_user_cache.keys():
+            continue
+        # try:
+        #     # 如果该用户已被访问过
+        #     fans_list = visited_user_cache[user_id]
+        # except KeyError:
+        else:
             since_id = 0
             fans_list = []
             while True:
@@ -105,7 +120,7 @@ while search_depth <= 1:
                     break
                 else:
                     since_id += 20
-            user_info_cache[user_id] = fans_list
-        fans_info.append({'user_id': user_id, 'fans_info': fans_list})
+            visited_user_cache[user_id] = fans_list
+            fans_info.append({'user_id': user_id, 'fans_info': fans_list})
         
 # https://m.weibo.cn/api/container/getIndex?containerid=231051_-_followers_-_3479691367&page=2
