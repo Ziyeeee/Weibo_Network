9ad210bf44b6008d4e23d06bbda559bf6d209cb5
Ziye <504250110@qq.com>
2021-10-12 01:53:16 +0800

    引入缓存

--- util/spider.py
+++ util/spider.py
@@ -3,24 +3,26 @@ import json
 import time
 from queue import Queue
 
-
 """
     网址：https://m.weibo.cn/profile/3479691367
     待完成：
-        1.中断后从断点开始爬取
-        2.爬取关注的人
+        1.爬取关注的人
 """
+
 
 def repeat_request(url):
     response = requests.request("GET", url)
     data = response.content
     data = json.loads(data)
     if data['ok'] == 0:
+        # 未获取到数据
         return data, False
     elif 'user' not in data['data']['cards'][-1]['card_group'][0].keys():
+        # 数据不完整
         return data, False
     else:
         return data, True
+
 
 def spider_fans(user_id, since_id):
     fans_info = []
@@ -28,9 +30,6 @@ def spider_fans(user_id, since_id):
     # url = "https://m.weibo.cn/api/container/getIndex"
     url = "https://m.weibo.cn/api/container/getIndex?containerid=231051_-_fans_-_{}&since_id={}" \
         .format(user_id, since_id)
-
-    # cookie = "WEIBOCN_FROM=1110006030; SUB=_2A25MZdyQDeRhGeFO7VAY9y3EyTqIHXVvqeTYrDV6PUJbkdANLXHNkW1NQU89GizFRnsnG85Yx52afW4ioSYTBWNI; MLOGIN=1; _T_WM=73123320612; XSRF-TOKEN=c8dece; M_WEIBOCN_PARAMS=luicode=10000011&lfid=231051_-_fans_-_3479691367&fid=231051_-_fans_-_3479691367&uicode=10000011"
-    # headers = {'cookie': cookie}
 
     data, success_flag = repeat_request(url)
 
@@ -40,11 +39,10 @@ def spider_fans(user_id, since_id):
         print("Request Failed!!!\tWait {}s".format(wait_time))
         time.sleep(wait_time)
         data, success_flag = repeat_request(url)
-        if wait_time >= 37.5:
+        if wait_time >= 150:
             return [], True
         elif wait_time <= 150:
             wait_time *= 4
-
 
     # 获取粉丝列表，当被大V关注时，cards[0]会列举大V信息
     fans_list = data['data']['cards'][-1]['card_group']
@@ -54,21 +52,13 @@ def spider_fans(user_id, since_id):
         end = False
 
     for fan in fans_list:
-        try:
-            # 忽略粉丝数>500的用户
-            if fan['user'] is not None and \
-                    fan['user']['followers_count'].isdigit() and \
-                    int(fan['user']['followers_count']) <= 500:
-                try:
-                    # id、昵称、性别
-                    fans_info.append({'fan_id': fan['user']['id'], 'fan_name': fan['user']['screen_name'],
-                                      'fan_gender': fan['user']['gender']})
-                except:
-                    print("Error!!!", fan)
-                    continue
-        except:
-            print("Error!!!", fan)
-            continue
+        # 忽略粉丝数>500的用户
+        if fan['user'] is not None and \
+                fan['user']['followers_count'].isdigit() and \
+                int(fan['user']['followers_count']) <= 500:
+            # id、昵称、性别
+            fans_info.append({'fan_id': fan['user']['id'], 'fan_name': fan['user']['screen_name'],
+                              'fan_gender': fan['user']['gender']})
     return fans_info, end
 
 
@@ -80,6 +70,9 @@ user_ids.put(3479691367)
 queue_len = user_ids.qsize()
 search_depth = 0
 
+with open('../data/user_cahce.json', 'r') as cache_f:
+    user_cache = json.load(cache_f)
+
 while search_depth <= 5:
     if user_ids.empty():
         # 本层遍历结束
@@ -89,38 +82,40 @@ while search_depth <= 5:
         # 保存数据
         with open('../data/fans-depth{}.json'.format(search_depth), 'w') as f:
             json.dump(fans_info, f)
-        # with open('temp_user_ids.json', 'w') as temp_f:
-        #     temp_queue = copy.deepcopy(user_ids)
-        #     temp_ids = []
-        #     while not temp_queue.empty():
-        #         temp_ids.append(temp_queue.get())
-        #     json.dump(temp_ids, temp_f)
         fans_info = []
         print('Search Depth {} Finished'.format(search_depth))
         search_depth += 1
     else:
         user_id = user_ids.get()
         if user_id in visited_user_cache.keys():
+            # 如果该用户已被访问过
             continue
-        # try:
-        #     # 如果该用户已被访问过
-        #     fans_list = visited_user_cache[user_id]
-        # except KeyError:
         else:
-            since_id = 0
-            fans_list = []
-            while True:
-                time.sleep(1)
-                fans, end = spider_fans(user_id, since_id)
-                fans_list += fans
-                print('{}/{}:\t{}'.format(queue_len - user_ids.qsize(), queue_len, fans))
-                for fan in fans:
+            if str(user_id) in user_cache.keys():
+                # 如果该用户信息已被缓存
+                print('{}/{}:\t{} in cache'.format(queue_len - user_ids.qsize(), queue_len, user_id))
+                for fan in user_cache[str(user_id)]:
                     temp_user_ids.put(fan['fan_id'])
-                if end:
-                    break
-                else:
-                    since_id += 20
-            visited_user_cache[user_id] = fans_list
-            fans_info.append({'user_id': user_id, 'fans_info': fans_list})
-        
+                visited_user_cache[user_id] = user_cache[str(user_id)]
+                fans_info.append({'user_id': user_id, 'fans_info': user_cache[str(user_id)]})
+            else:
+                since_id = 0
+                fans_list = []
+                while True:
+                    # time.sleep(1)
+                    fans, end = spider_fans(user_id, since_id)
+                    fans_list += fans
+                    print('{}/{}:\t{}'.format(queue_len - user_ids.qsize(), queue_len, fans))
+                    for fan in fans:
+                        temp_user_ids.put(fan['fan_id'])
+                    if end:
+                        break
+                    else:
+                        since_id += 20
+                visited_user_cache[user_id] = fans_list
+                fans_info.append({'user_id': user_id, 'fans_info': fans_list})
+                # 缓存已经获取到的所有用户信息
+                with open('../data/user_cahce.json', 'w') as cache_f:
+                    json.dump(visited_user_cache, cache_f)
+
 # https://m.weibo.cn/api/container/getIndex?containerid=231051_-_followers_-_3479691367&page=2
